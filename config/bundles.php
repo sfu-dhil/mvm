4a69f35d38c91208a7c415762f5631cd946d8ef6
Michael Joyce <ubermichael@gmail.com>
2020-11-12 14:56:55 -0800

    clean up the bundle usage.

--- config/bundles.php
+++ config/bundles.php
@@ -18,9 +18,8 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Nines\BlogBundle\NinesBlogBundle::class => ['all' => true],
-    Nines\DublinCoreBundle\NinesDublinCoreBundle::class => ['all' => true],
     Nines\EditorBundle\NinesEditorBundle::class => ['all' => true],
-    Nines\FeedbackBundle\NinesFeedbackBundle::class => ['all' => true],
+    Nines\MakerBundle\NinesMakerBundle::class => ['dev' => true],
     Nines\UserBundle\NinesUserBundle::class => ['all' => true],
     Nines\UtilBundle\NinesUtilBundle::class => ['all' => true],
 ];
