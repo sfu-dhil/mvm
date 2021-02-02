3c0a94d31acf4207adfb05ad1f3a160ea8f54cb1
Joey Takeda <joey.takeda@gmail.com>
2021-02-01 18:05:56 -0800

    Assets and font config

--- public/js/mvm.js
+++ public/js/mvm.js
@@ -5,14 +5,47 @@
 * @started 2020
 *
 * */
-import Modals from '../yarn/dhilux/js/dist/modals.bundle.js'
 
-let entities = ['person','theme','region','period'];
-let query = entities.map(e => `a[href*="${e}"]`).join(', ');
-console.log(query);
-let modals = new Modals(query);
-modals.init();
-console.log(modals);
+import dialogPolyfill from '../yarn/dhilux/js/modals.bundle.js';
+import Modals from '../yarn/dhilux/js/modals.js';
+
+
+const docId = document.querySelector('html').id;
+const entities = ['person','theme','region','period','coterie', 'print_source', 'archive'].filter(isIndex);
+
+(function(){
+    if (!(/(edit|new)\/?$/gi.test(location) || docId == 'index')){
+        makeModals();
+    }
+})();
+
+
+function makeModals(){
+    let selector = entities.map(ent => `a[href*="${ent}"]`).join(', ');
+    let modals = new Modals(selector);
+    modals.el = document.querySelector('main');
+    modals.filterParentsSelector = '.gallery, html[id$="_index"], header, p';
+    modals.linkFilter = (link) => {
+        console.log(link);
+        let table = link.closest('table');
+        if (table){
+            let thead = table.querySelector('thead');
+            return (thead === null);
+        }
+    }
+    modals.debug = true;
+    modals.init();
+}
+
+function isIndex(entity) {
+    let rex = new RegExp(`\\/${entity}\\/?$`, 'gi');
+    return !(rex.test(window.location.href));
+}
+
+
+
+
+
 
 
 
